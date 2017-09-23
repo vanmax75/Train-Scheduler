d2090b693f790d10b916c41bd43107b63db2a087
vanmax75 <zaksang@gmail.com>
2017-09-23 00:56:30 -0700

    init

---
+++ assets/javascript.js
@@ -0,0 +1,83 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBK99I3Za8NTNE7o_dgBWrvMIxHjHx0XVI",
+    authDomain: "trainsch-w7.firebaseapp.com",
+    databaseURL: "https://trainsch-w7.firebaseio.com/",
+    storageBucket: "trainsch-w7.appspot.com",
+    messagingSenderId: "581034538339"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var currentTime = moment();
+
+database.ref().on("child_added", function(childSnap) {
+
+    var name = childSnap.val().name;
+    var destination = childSnap.val().destination;
+    var firstTrain = childSnap.val().firstTrain;
+    var frequency = childSnap.val().frequency;
+    var min = childSnap.val().min;
+    var next = childSnap.val().next;
+
+    $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
+});
+
+database.ref().on("value", function(snapshot) {
+   
+
+});
+
+//get information from the form
+$("#addTrainBtn").on("click", function() {
+
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = $("#firstInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
+
+    //Input value Verification
+    if (trainName == "") {
+        alert('Enter a train name.');
+        return false;
+    }
+    if (destination == "") {
+        alert('Enter a destination.');
+        return false;
+    }
+    if (firstTrain == "") {
+        alert('Enter a first train time.');
+        return false;
+    }
+    if (frequency == "") {
+        alert('Enter a frequency');
+        return false;
+    }
+
+    
+    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+    // time difference current time / first train
+    var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    var remainder = difference % frequency;
+    var minUntilTrain = frequency - remainder;
+    var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        min: minUntilTrain,
+        next: nextTrain
+    }
+
+    console.log(newTrain);
+    database.ref().push(newTrain);
+
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstInput").val("");
+    $("#frequencyInput").val("");
+
+    return false;
+});
